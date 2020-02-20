c18dc079309814af86396af03d07942298ca1c52
Михаил Кузнецов <mk.kuzmich1974@yandex.ru>
2020-02-20 19:58:22 +0300

    Добавьте файлы проекта.

---
+++ Geniy_Idiot_NEW_Classes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Geniy_Idiot_NEW_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
